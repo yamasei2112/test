8229a3fc425a63192d5f7c4f4ecb3bc3deb0a581
yamasei2112 <yamasei2112@gmail.com>
2024-04-30 16:41:27 +0900

    env

--- go-rest-api/db/db.go
+++ go-rest-api/db/db.go
@@ -1,7 +1,18 @@
 package db
 
-import "gorm.io/gorm"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+)
 
 func NewDB() *gorm.DB {
-
+	if os.Getenv("GO_ENV") == "dev" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
 }
--- go-rest-api/go.mod
+++ go-rest-api/go.mod
@@ -2,8 +2,10 @@ module go-rest-api
 
 go 1.18
 
+require gorm.io/gorm v1.25.9
+
 require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	gorm.io/gorm v1.25.9 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 )
