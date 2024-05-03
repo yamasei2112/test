ba32614b954c1a7777ba6a1bb30fbb21497ca973
yamasei2112 <yamasei2112@gmail.com>
2024-05-04 05:28:50 +0900

    db tuika

--- go-rest-api/db/db.go
+++ go-rest-api/db/db.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -14,5 +15,22 @@ func NewDB() *gorm.DB {
 		if err != nil {
 			log.Fatalln(err)
 		}
+	}
+	url := fmt.Sprintf("postgres://%s:%s@%s:%s", os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PW"), os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DB"))
+
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Connceted")
+	return db
+}
+
+func CloseDB(db *gorm.DB) {
+	sqlDB, _ := db.DB()
+	if err := sqlDB.Close(); err != nil {
+		log.Fatalln(err)
 	}
 }
--- go-rest-api/go.mod
+++ go-rest-api/go.mod
@@ -2,10 +2,12 @@ module go-rest-api
 
 go 1.18
 
-require gorm.io/gorm v1.25.9
+require (
+	github.com/joho/godotenv v1.5.1
+	gorm.io/gorm v1.25.9
+)
 
 require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 )
